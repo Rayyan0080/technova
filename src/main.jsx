d214e48a8941ceabfa19893f9e19af44ec62a8f2
Rayyan0080 <rayyanomarz@gmail.com>
2026-07-05 22:37:32 -0400

    aa

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom"; // Import this
 import App from "./App.jsx";
 import { CartProvider } from "./contexts/CartContext.jsx";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter basename="/technova">
+    {/* The Router must wrap everything */}
+    <BrowserRouter basename={import.meta.env.DEV ? "/" : "/technova/"}>
       <CartProvider>
         <App />
       </CartProvider>
